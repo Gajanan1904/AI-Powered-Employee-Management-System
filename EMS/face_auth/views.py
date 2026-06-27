2eeea35635392d726ef5c59ff2554fffc4b14041
Gajanan Bidwai <152101821+Gajanan1904@users.noreply.github.com>
2026-06-28 01:47:45 +0530

    Lazy load DeepFace

--- EMS/face_auth/views.py
+++ EMS/face_auth/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from deepface import DeepFace
 import tempfile
 import base64
 import os
@@ -14,13 +13,12 @@ def verify_face(request):
         return JsonResponse({"error": "POST required"})
 
     try:
+        # Import ONLY when this API is called
+        from deepface import DeepFace
 
         data = json.loads(request.body)
 
-        image_data = data["image"]
-
-        image_data = image_data.split(",")[1]
-
+        image_data = data["image"].split(",")[1]
         image_bytes = base64.b64decode(image_data)
 
         temp_file = tempfile.NamedTemporaryFile(
@@ -29,7 +27,6 @@ def verify_face(request):
         )
 
         temp_file.write(image_bytes)
-
         temp_file.close()
 
         result = DeepFace.verify(
@@ -37,8 +34,6 @@ def verify_face(request):
             img2_path="face_auth/known_faces/gajanan.jpg",
             enforce_detection=False
         )
-        
-        print(result)
 
         os.unlink(temp_file.name)
 
@@ -48,7 +43,4 @@ def verify_face(request):
         })
 
     except Exception as e:
-
-        return JsonResponse({
-            "error": str(e)
-        })
+        return JsonResponse({"error": str(e)})
